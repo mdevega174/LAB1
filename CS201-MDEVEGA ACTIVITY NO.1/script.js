28a53454c7146b70f2603a75b5519b1fc04642da
mdevega174 <147475153+mdevega174@users.noreply.github.com>
2024-01-07 19:59:52 +0800

    Add files via upload

---
+++ CS201-MDEVEGA ACTIVITY NO.1/script.js
@@ -0,0 +1,11 @@
+let firstFavNum = 204;
+let secondFavNum = 107;
+
+console.log(firstFavNum + secondFavNum);
+console.log(firstFavNUm - secondFavNum);
+console.log(firstFavNUm * secondFavNum);
+console.log(firstFavNUm / secondFavNum);
+console.log(firstFavNUm % secondFavNum);
+let power = Math.pow(firstFavNum, 1);
+console.log(power);
+
